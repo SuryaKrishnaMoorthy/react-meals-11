ca2b41fda5f4a5e4bae12cde58f0d7d8de0c7bda
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2021-10-26 12:08:13 -0400

    add logic for fetching meals from firebase, loading and error handling

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,60 @@
+import { useEffect, useState } from 'react';
+
 import classes from './AvailableMeals.module.css';
 import Card from '../UI/Card/Card';
 import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
-	{
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-]
 
 const AvailableMeals = () => {
-	const mealsList = DUMMY_MEALS.map(meal => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+
+  useEffect(() => { 
+  
+    const fetchMeals = async () => {
+      const response = await fetch('https://task-tracker-15-http-default-rtdb.firebaseio.com/meals.json');
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!')
+      };
+
+      const data = await response.json();
+
+
+      const loadedMeals = [];
+
+      for(const key in data) {
+        loadedMeals.push({id: key, ...data[key]});
+      }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    }
+
+    fetchMeals().catch(err =>  {
+      setIsLoading(false);
+      setHttpError(err.message);
+    });
+  }, []);
+
+  if(isLoading) {
+    return (
+      <section className={classes.loading}>
+        <p>Loading...</p>
+      </section>
+    )
+  }
+
+  if(httpError) {
+    return (
+      <section className={classes.error}>
+        <p>{httpError}</p>
+      </section>
+    )
+  }
+
+
+	const mealsList = meals.map(meal => (
     <MealItem 
       id={meal.id}
       key={meal.id}
